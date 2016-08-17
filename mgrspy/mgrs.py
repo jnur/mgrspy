e7b853e887d647439c884b7733dcfda3da5b3a7a
Alexander Bruy <alexander.bruy@gmail.com>
2016-08-17 12:43:09 +0300

    conversion from UTM to MGRS (work in progress)

--- mgrspy/mgrs.py
+++ mgrspy/mgrs.py
@@ -44,12 +44,32 @@ MAX_PRECISION = 5                    # Maximum precision of easting & northing
 MIN_EAST_NORTH = 0
 MAX_EAST_NORTH = 4000000
 
-UPS_Constants = {0: [LETTERS['A'], LETTERS['J'], LETTERS['Z'], LETTERS['Z'], 800000.0, 800000.0],
-                 1: [LETTERS['B'], LETTERS['A'], LETTERS['R'], LETTERS['Z'], 2000000.0, 800000.0],
-                 2: [LETTERS['Y'], LETTERS['J'], LETTERS['Z'], LETTERS['P'], 800000.0, 1300000.0],
-                 3: [LETTERS['Z'], LETTERS['A'], LETTERS['J'], LETTERS['P'], 2000000.0, 1300000.0]
+UPS_Constants = {0: (LETTERS['A'], LETTERS['J'], LETTERS['Z'], LETTERS['Z'], 800000.0, 800000.0),
+                 1: (LETTERS['B'], LETTERS['A'], LETTERS['R'], LETTERS['Z'], 2000000.0, 800000.0),
+                 2: (LETTERS['Y'], LETTERS['J'], LETTERS['Z'], LETTERS['P'], 800000.0, 1300000.0),
+                 3: (LETTERS['Z'], LETTERS['A'], LETTERS['J'], LETTERS['P'], 2000000.0, 1300000.0)
                 }
 
+Latitude_Bands = [(LETTERS['C'], 1100000.0, -72.0, -80.5, 0.0),
+                  (LETTERS['D'], 2000000.0, -64.0, -72.0, 2000000.0),
+                  (LETTERS['E'], 2800000.0, -56.0, -64.0, 2000000.0),
+                  (LETTERS['F'], 3700000.0, -48.0, -56.0, 2000000.0),
+                  (LETTERS['G'], 4600000.0, -40.0, -48.0, 4000000.0),
+                  (LETTERS['H'], 5500000.0, -32.0, -40.0, 4000000.0),
+                  (LETTERS['J'], 6400000.0, -24.0, -32.0, 6000000.0),
+                  (LETTERS['K'], 7300000.0, -16.0, -24.0, 6000000.0),
+                  (LETTERS['L'], 8200000.0, -8.0, -16.0, 8000000.0),
+                  (LETTERS['M'], 9100000.0, 0.0, -8.0, 8000000.0),
+                  (LETTERS['N'], 0.0, 8.0, 0.0, 0.0),
+                  (LETTERS['P'], 800000.0, 16.0, 8.0, 0.0),
+                  (LETTERS['Q'], 1700000.0, 24.0, 16.0, 0.0),
+                  (LETTERS['R'], 2600000.0, 32.0, 24.0, 2000000.0),
+                  (LETTERS['S'], 3500000.0, 40.0, 32.0, 2000000.0),
+                  (LETTERS['T'], 4400000.0, 48.0, 40.0, 4000000.0),
+                  (LETTERS['U'], 5300000.0, 56.0, 48.0, 4000000.0),
+                  (LETTERS['V'], 6200000.0, 64.0, 56.0, 6000000.0),
+                  (LETTERS['W'], 7000000.0, 72.0, 64.0, 6000000.0),
+                  (LETTERS['X'], 7900000.0, 84.5, 72.0, 6000000.0)]
 
 class MgrsException(Exception):
     pass
@@ -85,7 +105,14 @@ def wgsToMgrs(latitude, longitude, precision):
         mgrs = upsToMgrs(hemisphere, x, y, precision)
     else:
         # Convert to UTM
-        pass
+        hemisphere, zone, epsg = _epsgForWgs(latitude, longitude)
+        src = osr.SpatialReference()
+        src.ImportFromEPSG(4326)
+        dst = osr.SpatialReference()
+        dst.ImportFromEPSG(epsg)
+        ct = osr.CoordinateTransformation(src, dst)
+        x, y, z = ct.TransformPoint(longitude, latitude)
+        mgrs = utmToMgrs(zone, hemisphere, latitude, longitude, x, y, precision)
 
     return mgrs
 
@@ -166,8 +193,55 @@ def upsToMgrs(hemisphere, easting, northing, precision):
 
     return mgrsString(0, mgrsLetters, easting, northing, precision)
 
-def utmToMgrs():
-    pass
+
+def utmToMgrs(zone, hemisphere, latitude, longitude, easting, northing, precision):
+    """ Calculates an MGRS coordinate string based on the UTM zone, latitude,
+    easting and northing values.
+
+    @param zone - UTM zone number
+    @param hemisphere - hemisphere either 'N' or 'S'
+    @param latitude - latitude value
+    @param longitude - longitude value
+    @param easting - easting/X in meters
+    @param northing - northing/Y in meters
+    @param precision - precision level of MGRS string
+    @returns - MGRS coordinate string
+    """
+    # Special check for rounding to (truncated) eastern edge of zone 31V
+    # TODO: do we really need this?
+    if (zone == 31) and (((latitude >= 56.0) and (latitude < 64.0)) and ((longitude >= 3.0) or (easting >= 500000.0))):
+        pass
+
+    if latitude <= 0.0 and northing == 1.0e7:
+        latitude = 0
+        northing = 0
+
+    ltr2LowValue, ltr2HighValue, patternOffset = _gridValues(zone)
+
+    mgrsLetters = [_latitudeLetter(latitude), None, None]
+
+    while northing >= TWOMIL:
+        northing = northing - TWOMIL
+
+    northing += patternOffset
+    if northing >= TWOMIL:
+        northing = northing - TWOMIL
+
+    mgrsLetters[2] = int(northing / ONEHT)
+    if mgrsLetters[2] > LETTERS['H']:
+        mgrsLetters[2] += 1
+
+    if mgrsLetters[2] > LETTERS['N']:
+        mgrsLetters[2] += 1
+
+    if ((mgrsLetters[0] == LETTERS['V']) and (zone == 31)) and (easting == 500000.0):
+        easting = easting - 1.0  # Substract 1 meter
+
+    mgrsLetters[1] = ltr2LowValue + int((easting / ONEHT) - 1)
+    if ltr2LowValue == LETTERS['J'] and mgrsLetters[1] > LETTERS['N']:
+        letters[1] += 1
+
+    return mgrsString(zone, mgrsLetters, easting, northing, precision)
 
 
 def mgrsString(zone, mgrsLetters, easting, northing, precision):
@@ -260,3 +334,49 @@ def _epsgForWgs(latitude, longitude):
         ns = 700
 
     return hemisphere, zone, 32000 + ns + zone
+
+
+def _gridValues(zone):
+    """ Sets the letter range used for the 2nd letter in the MGRS coordinate
+    string, based on the set number of the UTM zone. It also sets the pattern
+    offset using a value of A for the second letter of the grid square, based
+    on the grid pattern and set number of the UTM zone.
+
+    @param zone - UTM zone number
+    @returns - tuple containing 2nd letter low number, 2nd letter high number
+    and pattern offset
+    """
+    setNumber = zone % 6
+
+    if not setNumber:
+        setNumber = 6
+
+    if setNumber in [1, 4]:
+        ltr2LowValue = LETTERS['A']
+        ltr2HighValue = LETTERS['H']
+    elif setNumber in [2, 5]:
+        ltr2LowValue = LETTERS['J']
+        ltr2HighValue = LETTERS['R']
+    elif setNumber in [3, 6]:
+        ltr2LowValue = LETTERS['S']
+        ltr2HighValue = LETTERS['Z']
+
+    if setNumber % 2:
+        patternOffset = 0.0
+    else:
+        patternOffset = 500000.0
+
+    return ltr2LowValue, ltr2HighValue, patternOffset
+
+
+def _latitudeLetter(latitude):
+    """ Returns the latitude band letter for given latitude
+
+    @param latitude - latitude value
+    @returns - latitude band letter
+    """
+    if latitude >= 72 and latitude < 84.5:
+        return LETTERS['X']
+    elif latitude > -80.5 and latitude < 72:
+        idx = int(((latitude + 80.0) / 8.0) + 1.0e-12)
+        return Latitude_Bands[idx][0]
