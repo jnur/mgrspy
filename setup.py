e20fb59f014a5affb645ec8336e2842401c836c7
Alexander Bruy <alexander.bruy@gmail.com>
2016-08-19 15:37:09 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+from setuptools import setup
+
+with codecs.open('README', encoding='utf-8') as f:
+    readme_text = f.read()
+
+
+setup(
+    name="mgrspy",
+    version="0.1",
+    install_requires=['GDAL>=2.0.0'],
+    author="Alexander Bruy",
+    author_email="abruy@boundlessgeo.com",
+    description="Convert WGS84 coordinates to MGRS and back",
+    long_description=(readme_text),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: GIS'
+    ],
+    license="GPL",
+    keywords="mgrs wgs gis coordinate conversion",
+    url='https://github.com/boundlessgeo/mgrspy',
+    package_dir={'': '.'},
+    test_suite='test.suite',
+    packages=['mgrspy',]
+)
